ec1083e454609b948db3606adeade75742684f08
hao-udoin <haoyuetang00@gmail.com>
2024-12-15 01:23:58 -0500

    uniformize into base option class + derived classes for different option types

---
+++ 12_14_updates/OptionBase.h
@@ -0,0 +1,26 @@
+#ifndef OPTION_BASE_H
+#include OPTION_BASE_H
+
+class OptionBase {
+    protected:
+        bool isCall; // true for call, false for put
+        double s; // the current underlying price
+        double k; // the strike price
+        double t; // the time to expiration (in years)
+        double sigma; // the volatility of the stock price (per annum)
+        double r; // the risk-free interest rate (per annum)
+        double q=0; // the rate of dividend yield (assumed continuous, per annum)
+    
+    public:
+        OptoinBase(double s, double k, double t, double sigma, double r, double q)
+            : s(s), k(k), t(t), sigma(sigma), r(r), q(q)
+        
+        virtual ~OptionBase() {}
+
+        virtual double price() const = 0;
+        virtual double delta;
+        virtual double gamma;
+        virtual double theta;
+        virtual double vega;
+};
+#endif
