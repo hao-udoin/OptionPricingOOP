7024034771e8b690444d1bb57649395087e1fa35
hao-udoin <haoyuetang00@gmail.com>
2024-12-15 13:33:01 -0500

    Update OptionBase.h

--- 12_14_updates/OptionBase.h
+++ 12_14_updates/OptionBase.h
@@ -1,5 +1,5 @@
 #ifndef OPTION_BASE_H
-#include OPTION_BASE_H
+#define OPTION_BASE_H
 
 class OptionBase {
     protected:
@@ -9,18 +9,20 @@ class OptionBase {
         double t; // the time to expiration (in years)
         double sigma; // the volatility of the stock price (per annum)
         double r; // the risk-free interest rate (per annum)
-        double q=0; // the rate of dividend yield (assumed continuous, per annum)
+        double q; // the rate of dividend yield (assumed continuous, per annum)
     
     public:
-        OptoinBase(double s, double k, double t, double sigma, double r, double q)
-            : s(s), k(k), t(t), sigma(sigma), r(r), q(q)
+        OptionBase(bool isCall, double s, double k, double t, double sigma, double r, double q)
+            : isCall(isCall), s(s), k(k), t(t), sigma(sigma), r(r), q(q) {}
         
         virtual ~OptionBase() {}
-
+        
+        /*
         virtual double price() const = 0;
-        virtual double delta;
-        virtual double gamma;
-        virtual double theta;
-        virtual double vega;
+        virtual double delta();
+        virtual double gamma();
+        virtual double theta();
+        virtual double vega();
+        */
 };
 #endif
